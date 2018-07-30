f70ae94751232ad5ebbeaf8008aa3a778b4dda83
Thacher Hussain <thachert1d@gmail.com>
2018-07-29 21:52:08 -0700

    AlbumList Component Update

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -3,43 +3,28 @@ import { View, Text } from 'react-native';
 import axios from 'axios';
 import AlbumDetail from './AlbumDetail';
 
-// class AlbumList extends Component {
-//   state = { albums: [] };
-//
-//   componentWillMount() {
-//     axios.get('https://rallycoding.herokuapp.com/api/music_albums')
-//       .then(response => this.setState({ albums: response.data }));
-//   }
-//
-//   renderAlbums() {
-//     return this.state.albums.map(album =>
-//       <AlbumDetail key={album.title} album={album} />
-//     );
-//   }
-//
-//   render() {
-//     console.log(this.state);
-//     return (
-//       <View>
-//         {this.renderAlbums()}
-//       </View>
-//     );
-//   }
-// }
-
 class AlbumList extends Component {
+  state = { albums: [] };
+
   componentWillMount() {
-    axios.get('https://thachert1d-nightscout.herokuapp.com/api/v1/devicestatus')
-      .then(response => console.log(response.data));
+    axios.get('https://rallycoding.herokuapp.com/api/music_albums')
+      .then(response => this.setState({ albums: response.data }));
   }
+
+  renderAlbums() {
+    return this.state.albums.map(album =>
+      <AlbumDetail key={album.title} album={album} />
+    );
+  }
+
   render() {
+    console.log(this.state);
     return (
       <View>
-        <Text>Hello</Text>
+        {this.renderAlbums()}
       </View>
     );
   }
 }
-
 
 export default AlbumList;
